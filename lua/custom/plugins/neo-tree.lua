4446638d81332046a3794f37f7e436187c6321ac
Love Arreborn <love.arreborn@liu.se>
2024-11-25 13:46:05 +0100

    removed auto-comments in init.lua

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -4,18 +4,15 @@ return {
   cmd = 'Neotree',
   init = function()
     vim.api.nvim_create_autocmd('BufEnter', {
-      -- make a group to be able to delete it later
       group = vim.api.nvim_create_augroup('NeoTreeInit', { clear = true }),
       callback = function()
         local f = vim.fn.expand '%:p'
         if vim.fn.isdirectory(f) ~= 0 then
           vim.cmd('Neotree focus dir=' .. f)
-          -- neo-tree is loaded now, delete the init autocmd
           vim.api.nvim_clear_autocmds { group = 'NeoTreeInit' }
         end
       end,
     })
-    -- keymaps
   end,
   keys = {
     { '<leader>e', ':Neotree toggle<CR>', desc = 'Toggle file explorer', silent = true },
@@ -36,8 +33,7 @@ return {
   },
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'nvim-tree/nvim-web-devicons',
     'MunifTanjim/nui.nvim',
-    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
 }
--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -5,6 +5,9 @@ return {
     opts = {
       direction = 'float',
       open_mapping = [[<leader>v]],
+      float_opts = {
+        border = 'curved',
+      },
     },
   },
 }
