b56c5fa5facdbf581c7e3a1883abebffdddf5635
Love Arreborn <love.arreborn@liu.se>
2025-01-20 08:33:12 +0100

    refactored init.lua to modularize all plugins

---
+++ lua/kickstart/plugins/1_sleuth.lua
@@ -0,0 +1,15 @@
+return {
+  'tpope/vim-sleuth',
+  {
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '' },
+        change = { text = '󰘙' },
+        delete = { text = '󱖟' },
+        topdelete = { text = '󱉇' },
+        changedelete = { text = '󱟃' },
+      },
+    },
+  },
+}
