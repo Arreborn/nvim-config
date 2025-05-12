7f6e7ab9c05ccbbedd82ad7dc6d65d1c601b1e78
Love Arreborn <love.arreborn@liu.se>
2025-05-12 12:47:42 +0200

    changed a color because why not

--- lua/custom/plugins/flash.lua
+++ lua/custom/plugins/flash.lua
@@ -44,9 +44,9 @@ return {
     },
   },
   config = function()
-    vim.api.nvim_set_hl(0, 'FlashCurrent', { bold = false, fg = '#FFFFFF' })
-    vim.api.nvim_set_hl(0, 'FlashMatch', { bold = false, fg = '#FFFFFF' })
-    vim.api.nvim_set_hl(0, 'FlashLabel', { bold = true, fg = '#FF0000', bg = '#404247' })
+    vim.api.nvim_set_hl(0, 'FlashCurrent', { bold = false, fg = '#d19a66' })
+    vim.api.nvim_set_hl(0, 'FlashMatch', { bold = false, fg = '#d19a66' })
+    vim.api.nvim_set_hl(0, 'FlashLabel', { bold = true, fg = '#98c379', bg = '#404247' })
     vim.api.nvim_set_hl(0, 'FlashBackdrop', { italic = false, fg = '#646568' })
   end,
 }
