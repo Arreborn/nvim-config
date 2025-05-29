438fcf11847a55b830c1d096fbc6696e6f6cf065
Love Arreborn <love.arreborn@liu.se>
2025-05-29 11:36:17 +0200

    Adding Neogit back into the fold

---
+++ lua/custom/plugins/neogit.lua
@@ -0,0 +1,12 @@
+return {
+  'NeogitOrg/neogit',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'sindrets/diffview.nvim',
+    'folke/snacks.nvim',
+  },
+  keys = {
+    { '<leader>gg', '<cmd>Neogit<cr>', desc = 'Git Status' },
+    { '<leader>gc', '<cmd>Neogit commit<cr>', desc = 'Git Commit' },
+  },
+}
